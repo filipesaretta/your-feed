396b058f3b92c481103bd26a2aa8c29a3f734af1
filipesaretta <filipessm124@gmail.com>
2022-06-09 15:27:35 -0300

    Finish sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,6 @@
 import styles from './sidebar.module.scss';
+import { PencilLine } from "phosphor-react";
+
 
 export function Sidebar() {
   return (
@@ -6,13 +8,15 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1445560302902-faf947ee9413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bW91bnRhaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
+        <img src="https://github.com/filipesaretta.png" />
         <strong>Filipe Saretta</strong>
         <span>Front-end Developer</span>
       </div>
 
       <footer className={styles.footer}>
-        <a href="#">Edit Profile</a>
+
+        <a href="#"><PencilLine size={20} />Edit Profile</a>
       </footer>
-    </aside >
+    </aside>
   )
 }
